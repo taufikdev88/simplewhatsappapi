2b93c8ba8d854e59045ee8ee15c3fb78ff4adcf2
Taufik Hidayat <taufikdev88@gmail.com>
2022-03-27 12:34:25 +0700

    Tambah qrcode gambar

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const { WhatsappService } = require('./services/WhatsappService');
 const express = require('express');
 const { MessageWrite } = require('./rules/MessageWrite');
 const validator = require('./helper/Validator');
+const QRCode = require('qrcode')
 
 const app = express();
 const wa = new WhatsappService();
@@ -23,10 +24,34 @@ app.get('/status', (req, res) => {
     res.send(wa.GetStatus());
 });
 
+app.get('/qr', (req, res) => {
+    const body = "<html><head><title>Whatsapp QrCode</title><script>setTimeout(function(){window.location.reload();}, 1000);</script></head><body>@body</body></html>";
+
+    const status = wa.GetStatus();
+    if (status.isConnected){
+        const response = "<div><h1 style='text-align: center;' >Whatsapp Connected to " + status.phoneNumber + "</h1></div>";
+        const result = body.replace('@body', response);
+        res.send(result);
+    } else {
+        QRCode.toDataURL(wa.qrcode, (err, code) => {
+            if (err) {
+                const response = "<div><h1 style='text-align: center;' >" + err.message + "</h1></div></div>";
+                const result = body.replace('@body', response);
+                res.send(result);
+            } else {
+                const response = "<div><img style='display: block; margin-left: auto; margin-right: auto; width: 30%;' src='" + code + "' alt='whatsapp qrcode' /></div>"
+                const result = body.replace('@body', response);
+                res.send(result);
+            }
+        });
+    }
+});
+
 app.post('/message', (req, res) => {
     validator(req.body, MessageWrite, {}, (error, isValid) => {
         if (!isValid){
-            res.status(400).send({
+            res.status(400);
+            res.send({
                 status: "failed",
                 errors: error.errors
             });
